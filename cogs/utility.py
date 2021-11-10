a8d1ef40a0e89415c84b5e26b0264394b14562ce
Dramizz <54241684+Dramizz@users.noreply.github.com>
2021-11-11 00:40:51 +0100

    test fix

--- cogs/utility.py
+++ cogs/utility.py
@@ -488,7 +488,7 @@ class Utility(commands.Cog):
 
     @commands.command(aliases=["presence"])
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def activity(self, ctx, activity_type: str.lower, *, message: str = ""):
+    async def activity(self, ctx, activity_type: str.lower, *, message: str = "DM Moi"):
         """
         Set an activity status for the bot.
 
